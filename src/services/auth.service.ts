e764251b6d2f1a099af1d0ee2dba9db800b2593e
Diaz Kautsar <diazkautsar77@gmail.com>
2026-01-01 21:21:34 +0700

    select role service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,14 @@
-import { getRoleBasedOnUserId, getUserBasedUsername } from "../models/index.js";
-import { HttpError, comparePassword, generateRoleSelectionToken } from "../helpers/index.js";
+import { getMenuBasedOnRoleId, getRoleBasedOnUserId, getRoleBaseId, getUserBasedOnUserId, getUserBasedUsername } from "../models/index.js";
+import { HttpError, comparePassword, generateAccessToken, generateRoleSelectionToken, validateRoleSelectionToken } from "../helpers/index.js";
 
 interface LoginInterface {
   username: string;
   password: string;
+}
+
+interface SubmitRoleLoginInterface {
+  roleSelectionToken: string;
+  selectedRoleId: number
 }
 
 export const login = async (dto: LoginInterface) => {
@@ -23,12 +28,47 @@ export const login = async (dto: LoginInterface) => {
     }
 
     const roles = await getRoleBasedOnUserId(selectedUser.id)
-    const rolesIds = roles.map(item => item.roleId)
-    const roleSelectionToken = generateRoleSelectionToken(userId, rolesIds)
+    const roleIds = roles.map(item => item.roleId)
+    const roleSelectionToken = generateRoleSelectionToken({ userId, roleIds })
 
     return {
       roles: roles.map(item => ({ value: item.roleId, label: item.roleName })),
       roleSelectionToken,
+    }
+  } catch (error) {
+    throw error
+  }
+}
+
+export const selectRoleLogin = async (dto: SubmitRoleLoginInterface) => {
+  try {
+    const { roleSelectionToken, selectedRoleId } = dto
+
+    const data = validateRoleSelectionToken(roleSelectionToken)
+    if (!data.roleIds.includes(selectedRoleId)) {
+      throw new HttpError(400, "role not found")
+    }
+
+    const [ role ] = await getRoleBaseId(selectedRoleId)
+    if (!role) {
+      throw new HttpError(400, "role not found")
+    }
+
+    const menu = await getMenuBasedOnRoleId(role.id)
+    const [ user ] = await getUserBasedOnUserId(data.userId)
+    if (!user) {
+      throw new HttpError(400, "Role selection token expired or invalid")
+    }
+
+    const accessToken = generateAccessToken({
+      userId: user.id,
+      firstName: user.first_name,
+      lastName: user.last_name,
+    })
+
+    return {
+      accessToken,
+      menu
     }
   } catch (error) {
     throw error
