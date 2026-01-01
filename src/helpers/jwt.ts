41b753ccf8a18853eef010e383821602ae2b8d0d
Diaz Kautsar <diazkautsar77@gmail.com>
2026-01-01 21:03:31 +0700

    add genearte accesss token

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,15 +1,48 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
+import { HttpError } from "./error.js";
 
-export const generateRoleSelectionToken = (userId: number, roleIds: number[]) => {
+interface RoleSelectionTokenInterface {
+  userId: number;
+  roleIds: number[];
+}
+
+interface AccessTokenInterface {
+  userId: number;
+  firstName: string;
+  lastName: string;
+}
+
+export const generateRoleSelectionToken = (dto: RoleSelectionTokenInterface) => {
   return jwt.sign(
     {
-      userId,
-      roleIds
+      ...dto
     },
     env.ROLE_SELECTION_SECRET,
     {
       expiresIn: "15m",
     }
   );
+}
+
+export const validateRoleSelectionToken = (token: string): RoleSelectionTokenInterface => {
+  try {
+    const payload = jwt.verify(token, env.ROLE_SELECTION_SECRET) as RoleSelectionTokenInterface
+
+    return payload
+  } catch (error) {
+    throw new HttpError(400, "Role selection token expired or invalid")
+  }
+}
+
+export const generateAccessToken = (dto: AccessTokenInterface) => {
+  return jwt.sign(
+    {
+      ...dto
+    },
+    env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1H"
+    }
+  )
 }
