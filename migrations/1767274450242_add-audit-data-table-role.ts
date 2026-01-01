63086c0c384bb5339bc9f4e51f8705568361e7a0
Diaz Kautsar <diazkautsar77@gmail.com>
2026-01-01 21:02:01 +0700

    add audit data to menu table

---
+++ migrations/1767274450242_add-audit-data-table-role.ts
@@ -0,0 +1,23 @@
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumns("menu", {
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: false,
+    },
+    deleted_at: {
+      type: 'timestamp',
+      notNull: false,
+    }
+  })
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
