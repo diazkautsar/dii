439fb7d1c314e410e25432fcdfeb14ebcd25faae
Diaz Kautsar <diazkautsar77@gmail.com>
2026-01-01 21:21:17 +0700

    auth select role

--- src/routers/auth.route.ts
+++ src/routers/auth.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { loginController } from "../controllers/index.js"
+import { loginController, selectRoleController } from "../controllers/index.js"
 
 const router = express.Router()
 
 router.post("/signin", loginController)
+router.post("/signin/role", selectRoleController)
 
 export { router as authRouter }
